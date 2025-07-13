b94ef7da746925681bc68d5d7655f9af64233a87
raguladhithyam <mra20031006@gmail.com>
2025-07-14 00:58:57 +0530

    db stuff updated

--- src/config/config.js
+++ src/config/config.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'ragul',
-    password: process.env.DB_PASSWORD || 'mynameisragul1@',
-    database: process.env.DB_NAME || 'blood_request_db',
-    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    logging: console.log
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: process.env.DB_USER || 'ragul',
